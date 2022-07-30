6eaee58a85327cafb484deecf1aa42b5cf156d6d
Aryan <2002ak2002@gmail.com>
2022-07-30 17:11:57 +0530

    second commit

--- portfo/models.py
+++ portfo/models.py
@@ -35,6 +35,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
         
-    def get_absolute_url(self):
-        return reverse("post_Detail", kwargs={"slug": self.slug})
+
 
